b7fa7ae8470aa1c485738d012f24f6d57b49f74c
emilykohlberg <emilykohlberg@dhcp-10-105-31-122.wireless.northwestern.private>
2023-05-31 09:44:24 -0500

    adding button

--- src/pages/ProcessingPage.jsx
+++ src/pages/ProcessingPage.jsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import "./ProcessingPage.css";
+
 const ProcessingPage = ({}) => {
+  const q1 = {
+    q: "If we have access to all of the training data and the model, which data processing method should we use?",
+    a: "Pre-Processing",
+  };
+  const [question, setQuestion] = useState("q");
+
+  const handleClick = () => {
+    if (question == "q") {
+      setQuestion("a");
+    } else {
+      setQuestion("q");
+    }
+  };
   return (
     <div className="processing-page">
       <h1>Addressing Algoirthmic Bias</h1>
@@ -11,7 +27,10 @@ const ProcessingPage = ({}) => {
         </div>
         <div className="processing-subsection">
           <h2>Pre-Processing</h2>
-          <h4>For when bias coming from training data</h4>
+          <h4>
+            For when bias coming from training data and you have access to the
+            data
+          </h4>
           <p>Training data can be biased in many ways.</p>
           <ul>
             <li>Groups of people can be under-represented</li>
@@ -19,67 +38,33 @@ const ProcessingPage = ({}) => {
               Labels for the training data are created by people, these labels
               can reflect that person's individual biases
             </li>
-            <li></li>
           </ul>
-          <p>Why is training data biased and how can we address it?</p>
+          <p>
+            This is when we make changes to the training data to make it more
+            fair.
+          </p>
         </div>
         <div className="processing-subsection">
           <h2>In-Processing</h2>
-          <p>
-            I will discuss algorithmic bias and fairness through an explainer
-            blog/website. This will be aimed towards high school or college
-            students early on in their computer science education or students
-            who do not plan on studying computer science. This is an important
-            and often overlooked computer science topic that I have only
-            recently seen in classes, but I think it would be better to learn
-            about earlier as fighting algorithmic bias should shape the way that
-            people program. Societal biases are often reflected in artificial
-            intelligence algorithms, whether this is intentional or
-            unintentional. Algorithmic bias is something that can be fixed,
-            which can lead to a more fair future for marginalized communities,
-            but people need to be educated on the topic in order to do this. The
-            intent behind the design will be to make people aware of the issue,
-            provide tangible examples so they can understand how it affects
-            people, and offer solutions that they can use in their own
-            programming. Because the target audience is not very familiar with
-            computer science, it will stay at a fairly high level instead of
-            going into the mechanics of different fairness metrics/algorithms.
-            For computer science students, it is meant to spark interest in the
-            topic early on in their education because I think it will help shape
-            their overall outlook on programming rather than feel like an
-            afterthought if it were to be presented later on in their careers.
-            For non-computer science students, it is meant to teach them about
-            the social implications that algorithmic bias has on the world.
-          </p>
+          <h4>
+            For when bias coming from the model and you have access to the model
+          </h4>
         </div>
         <div className="processing-subsection">
           <h2>Post-Processing</h2>
-          <p>
-            I will discuss algorithmic bias and fairness through an explainer
-            blog/website. This will be aimed towards high school or college
-            students early on in their computer science education or students
-            who do not plan on studying computer science. This is an important
-            and often overlooked computer science topic that I have only
-            recently seen in classes, but I think it would be better to learn
-            about earlier as fighting algorithmic bias should shape the way that
-            people program. Societal biases are often reflected in artificial
-            intelligence algorithms, whether this is intentional or
-            unintentional. Algorithmic bias is something that can be fixed,
-            which can lead to a more fair future for marginalized communities,
-            but people need to be educated on the topic in order to do this. The
-            intent behind the design will be to make people aware of the issue,
-            provide tangible examples so they can understand how it affects
-            people, and offer solutions that they can use in their own
-            programming. Because the target audience is not very familiar with
-            computer science, it will stay at a fairly high level instead of
-            going into the mechanics of different fairness metrics/algorithms.
-            For computer science students, it is meant to spark interest in the
-            topic early on in their education because I think it will help shape
-            their overall outlook on programming rather than feel like an
-            afterthought if it were to be presented later on in their careers.
-            For non-computer science students, it is meant to teach them about
-            the social implications that algorithmic bias has on the world.
-          </p>
+          <h4>
+            When you don't have access to either the model or the training data,
+            just the results.
+          </h4>
+        </div>
+        <div className="button-container">
+          <button className="question-button" onClick={handleClick}>
+            <p>{question == "q" ? "Question:" : "Answer:"}</p>
+            <p>
+              <b>{q1[question]}</b>
+            </p>
+            <p>Click to see {question == "q" ? "answer" : "question"}</p>
+          </button>
         </div>
       </div>
     </div>
