340ef05300d89c54492c2fa4d6520035f341e7a8
emilykohlberg <emilykohlberg@dhcp-10-105-31-122.wireless.northwestern.private>
2023-06-08 07:31:31 -0500

    references

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -17,6 +17,28 @@ const About = ({}) => {
           Science.
         </p>
       </div>
+      <div className="works-cited">
+        <b>References</b>
+        <p>
+          Bias. (n.d.). National Museum of African American History and Culture.
+          https://nmaahc.si.edu/learn/talking-about-race/topics/bias
+        </p>
+        <p>
+          Feast, J. (2020, October 30). Root Out Bias at Every Stage of Your
+          AI-Development Process. Harvard Business Review.
+          https://hbr.org/2020/10/root-out-bias-at-every-stage-of-your-ai-development-process
+        </p>
+        <p>
+          Shin, T. (2021, December 14). Real-life Examples of Discriminating
+          Artificial Intelligence. Medium.
+          https://towardsdatascience.com/real-life-examples-of-discriminating-artificial-intelligence-cae395a90070
+        </p>
+        <p>
+          Wang, X., Zhang, Y., & Zhu, R. (2022). A brief review on algorithmic
+          fairness. Management System Engineering, 1(1).
+          https://doi.org/10.1007/s44176-022-00006-z
+        </p>
+      </div>
     </div>
   );
 };
